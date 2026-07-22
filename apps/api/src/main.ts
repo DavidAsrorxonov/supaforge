31c95d4151797f82816d330ebbef7a0d5e840f01
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-07-22 10:51:31 +0900

    feat: implement authentication service with JWT, register and login DTOs

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,23 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  app.setGlobalPrefix("api");
+
+  app.use(cookieParser());
+
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      transform: true
-    })
-  )
+      transform: true,
+    }),
+  );
 
   app.enableCors({
-    origin: process.env.WEB_URL ?? "http://localhost:3001",
-    credentials: true, 
-  })
+    origin: process.env.WEB_URL ?? 'http://localhost:3001',
+    credentials: true,
+  });
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
